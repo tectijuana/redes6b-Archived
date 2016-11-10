888294345284151a2648e01ee8224d90c4d1e801
Santillan Gil Saul Ivan <saul.santillan@tectijuana.edu.mx>
2016-11-09 17:55:13 -0800

    Update Web Server Arduino.ino

--- SantillanSaul/Web Server Arduino.ino
+++ SantillanSaul/Web Server Arduino.ino
@@ -8,9 +8,9 @@
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
 byte mac[] = {
-  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+  0x10 0x08, 0xB1, 0x63, 0xB8, 0xAB
 };
-IPAddress ip(192, 168, 1, 50);
+IPAddress ip(172, 16, 20, 151);
 
 // Initialize the Ethernet server library
 // with the IP address and port you want to use

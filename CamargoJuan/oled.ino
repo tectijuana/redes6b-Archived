287cec62993140dd20a1e64dcf084a8cf1cbec89
pitin <juan.mc.diaz@gmail.com>
2016-12-01 16:45:50 -0800

    Update oled.ino

--- CamargoJuan/oled.ino
+++ CamargoJuan/oled.ino
@@ -1,15 +1,48 @@
-#include <Wire.h>
-#include "OzOLED.h"
+#include "U8glib.h"
 
-void setup(){
+U8GLIB_SSD1306_128X32 u8g(U8G_I2C_OPT_NONE);	// I2C / TWI 
 
-  OzOled.init();  //initialze Oscar OLED display
-  OzOled.printString("Hello World!"); //Print the String
 
+void draw(void) {
+  // graphic commands to redraw the complete screen should be placed here  
+  u8g.setFont(u8g_font_unifont);
+  //u8g.setFont(u8g_font_osb21);
+  u8g.drawStr( 0, 22, "Hello World!");
 }
 
-void loop(){
+void setup(void) {
+  // flip screen, if required
+  // u8g.setRot180();
   
+  // set SPI backup if required
+  //u8g.setHardwareBackup(u8g_backup_avr_spi);
+
+  // assign default color value
+  if ( u8g.getMode() == U8G_MODE_R3G3B2 ) {
+    u8g.setColorIndex(255);     // white
+  }
+  else if ( u8g.getMode() == U8G_MODE_GRAY2BIT ) {
+    u8g.setColorIndex(3);         // max intensity
+  }
+  else if ( u8g.getMode() == U8G_MODE_BW ) {
+    u8g.setColorIndex(1);         // pixel on
+  }
+  else if ( u8g.getMode() == U8G_MODE_HICOLOR ) {
+    u8g.setHiColorByRGB(255,255,255);
+  }
+  
+  pinMode(8, OUTPUT);
+}
+
+void loop(void) {
+  // picture loop
+  u8g.firstPage();  
+  do {
+    draw();
+  } while( u8g.nextPage() );
+  
+  // rebuild the picture after some delay
+  //delay(50);
 }
 
 
